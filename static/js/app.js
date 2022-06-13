f1db69407ebe287248eb84fc814fc4ea1d86866e
5t4david <81177362+5t4david@users.noreply.github.com>
2022-06-13 13:38:25 -0500

    read in json with d3

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+// use d3 library to read in samples.json
+// Initializing the page
+function initial_page() {
+
+    d3.json("samples.json").then(function(d){
+        var names = d.names;
+        var name_0 = names[0];
+
+        plotdata(name_0);
+        demographics(name_0)
+    }
+}
