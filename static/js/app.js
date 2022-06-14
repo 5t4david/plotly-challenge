a8f3e926b543dd3e38d0a2c95dae1bb3d1ee1aac
5t4david <81177362+5t4david@users.noreply.github.com>
2022-06-13 23:17:42 -0500

    building bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -2,11 +2,30 @@
 // Initializing the page
 function initial_page() {
 
-    d3.json("samples.json").then(function(d){
-        var names = d.names;
+    d3.json("samples.json").then(function(sd){
+        var names = sd.names;
         var name_0 = names[0];
 
-        plotdata(name_0);
-        demographics(name_0)
+        d3.select('#selDataset').selectAll('option')
+        .data(names)
+        .enter()
+        .append('option')
+        .attr('value', d => d)
+        .text(d => d);
+
+        buildPlots(name_0);
+        demographics(name_0);
+        
     })
+};
+
+function buildPlots(id) {
+    d3.json("samples.json").then(function(d){
+
+
+    })
+};
+function optionChanged(inID){
+    buildPlots(inID);
+    demographics(inID);
 };
