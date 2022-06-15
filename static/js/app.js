2762fc49decbaec9b7592b1314355d78ec2d3e8d
5t4david <81177362+5t4david@users.noreply.github.com>
2022-06-15 00:00:07 -0500

    plots

--- static/js/app.js
+++ static/js/app.js
@@ -7,25 +7,94 @@ function initial_page() {
         var name_0 = names[0];
 
         d3.select('#selDataset').selectAll('option')
-        .data(names)
-        .enter()
-        .append('option')
-        .attr('value', d => d)
-        .text(d => d);
+            .data(names)
+            .enter()
+            .append('option')
+            .attr('value', d => d)
+            .text(d => d);
 
         buildPlots(name_0);
         demographics(name_0);
         
     })
 };
-
+// use d3 library to read in samples.json
 function buildPlots(id) {
     d3.json("samples.json").then(function(d){
+        var ids = d.samples.filter(sample => sample.id == id);
+        var results = ids[0];
 
+        data = [];
+        for (i=0; i<results.sample_values.length; i++){
+            data.push({
+                id: `OTU ${results.otu_ids[i]}`,
+                value: results.sample_values[i],
+                label: results.otu_labels[i]
+            });
+        }
+        
+        var top_ten = data.sort(function sortFunction(a,b){
+            return b.value - a.value;
+        }).slice(0,10);
 
+        var hr_bar = top_ten.sort(function sortFunction(a,b){
+            return a.value - b.value;
+        })
+
+        var trace1 = [{
+            type: "bar",
+            orientation: 'h',
+            x: hr_bar.map(row=> row.value),
+            y: hr_bar.map(row => row.id),
+            text: hr_bar.map(row => row.label),
+            mode: 'markers',
+            marker: {
+                color: ['#316e5d', '#16b588', '#c9edf5', '#7885a1', '#033838', 
+                '#6efafa', '#066666', '#384a4a', '#c5e3e3', '#035057'],
+            }
+          }];
+
+        var Barlayout = {
+            title: `<span style='font-size:1em; color:#070f0d'><b>Top 10 OTUs for ID ${id}<b></span>`,
+            xaxis: {autorange: true, title: 'Sample Values'},
+            yaxis: {autorange: true},
+            width: 500,
+            height: 500
+            };
+
+        Plotly.newPlot("bar", trace1, Barlayout);
+
+        var trace2 = {
+            x: results.otu_ids,
+            y: results.sample_values,
+            mode: 'markers',
+            marker: {
+                size: results.sample_values,
+                color: results.otu_ids,
+                colorscale: 'country'
+            },
+            text: results.otu_labels
+        };
+
+        var Bubbledata = [trace2]
+
+        var Bubblelayout = {
+            title: `<span style='font-size:1em; color:#070f0d'><b>OTU Data for ID ${id}<b></span>`,
+            xaxis: {title:'OTU ID'},
+            yaxis: {title: 'Sample Values'},
+            width: window.width
+        };
+
+        Plotly.newPlot('bubble', Bubbledata, Bubblelayout);
+    
     })
 };
+
+
 function optionChanged(inID){
     buildPlots(inID);
     demographics(inID);
 };
+
+
+initial_page()
